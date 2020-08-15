a67dfe77aa53e834cb6993a1292687a4c3f441d7
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-08-15 18:34:24 +0900

    added logic to question card

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 
+type Props = {
+  question: string;
+  answers:  string[];
+  callback: any;
+  userAnswer: boolean;
+  questionNr: number;
+  totalQuestions: number;
+}
 
-const QuestionCard = () => <div> QuestionCard </div>
-
+const QuestionCard: React.FC<Props> = ({question, answers, callback, userAnswer, questionNr, totalQuestions}) => (
+  <div>
+    <p className="number">
+      Question: {questionNr} / {totalQuestions}
+    </p>
+    <p dangerouslySetInnerHTML={{ __html: question}} />
+    <div>
+      {answers.map(answer => (
+        <div>
+          <button disabled={userAnswer}>
+            <span dangerouslySetInnerHTML={{ __html: answer}} />
+          </button>
+        </div>
+        ))}
+    </div>
+  </div>
+);
 export default QuestionCard;
