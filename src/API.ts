ab04b6a590b0e491f5681e6f8dd901f9a1a30323
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-08-16 14:05:13 +0900

    added into api ts

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,17 @@
+import { shuffleArray } from './utils';
+
+export type Question = {
+  category: string;
+  correct_answer: string;
+  difficulty: string;
+  incorrect_answers: string[];
+  question: string;
+  type: string;
+}
+
+export type QuestionState = Question & { answers: string[]};
+
+
 export enum Difficulty {
   EASY = "easy",
   MEDIUM = "medium",
@@ -7,4 +21,8 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
   console.log(data);
+  return data.results.map((question: Question) => ({
+    ...question,
+    answer: shuffleArray([...question.incorrect_answers, question.correct_answer,]),
+  }));
 }
