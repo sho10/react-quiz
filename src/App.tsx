d087981ba56a23230921fa8a595b1d3f05e701b1
Shoichi Hasegawa <shoichihasegawa2018@hotmail.com>
2020-08-16 12:20:54 +0900

    app tsx added further logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import QuestionCard from './components/QuestionCard'
 
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
@@ -28,7 +29,14 @@ const App = () => {
       </button>
       <p className="score">Score:</p>
       <p>Loading Questions:</p>
-      <QuestionCard />
+      <QuestionCard
+        questionNr={number+1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      />
       <button className="next" onClick={nextQuestion}>
         Next Question
       </button>
